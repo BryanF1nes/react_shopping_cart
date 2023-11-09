4523c95ac8cfc366f86f9e99efd63f0ea26a3002
Bryan <bryanfinesw@gmail.com>
2023-11-09 15:16:48 -0500

    Upating Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,11 @@
 import { useOutletContext } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const Cart = () => {
     const [contextProps] = useOutletContext();
-    
+    const cartItems = contextProps.cart;
+    const { data, loading, error } = useFetch("https://fakestoreapi.com/products");
+
     return (
         <div>
             <p>This is the cart page</p>
